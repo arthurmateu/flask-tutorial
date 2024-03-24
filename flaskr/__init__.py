555b2ab9e1393ea2bca4184bc7297e87d9563faa
Arthur Mateu <arthurmateu@gmail.com>
2024-03-24 23:22:36 +0100

    Blog Blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -11,6 +11,9 @@ def create_app(test_config=None):
     db.init_app(app)
     from . import auth
     app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     app.config.from_mapping(
             SECRET_KEY='dev',
